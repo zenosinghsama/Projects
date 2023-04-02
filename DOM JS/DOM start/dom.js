43bbaf9305841fe950bd7ed6c1672baa8f1cb47b
zenosinghsama <iamarjunsingh007@gmail.com>
2023-04-02 15:33:58 +0530

    DOM 2

--- DOM JS/DOM start/dom.js
+++ DOM JS/DOM start/dom.js
@@ -22,7 +22,7 @@ for(let i = 0; i < li.length; i++) {
     li[i].style.fontWeight = 'bold';
 } */
 
-var item5 = document.getElementsByClassName('list-group') ;
+/* var item5 = document.getElementsByClassName('list-group') ;
 item5[0].style.backgroundColor = 'lightblue';
 
 var thirdItem =document.querySelector('.list-group-item:nth-child(3)');
@@ -35,4 +35,80 @@ var odd = document.querySelectorAll('li:nth-child(odd)');
 
 for(let i = 0; i < odd.length; i++){
     odd[i].style.backgroundColor = 'green';
-}
+} */
+
+
+// TRAVERSING THE DOM
+var itemlist = document.querySelector('#items');
+//parentNode
+/* console.log(itemlist.parentNode);
+itemlist.parentNode.style.backgroundColor = '#f4f4f4'; */
+
+// parentElement
+/* console.log(itemlist.parentElement);
+itemlist.parentElement.style.backgroundColor = '#f4f4f4'
+console.log(itemlist.parentElement.parentElement.parentNode);
+ */
+
+// CHILDNODES
+/* console.log(itemlist.childNodes); */
+/* 
+console.log(itemlist.children);
+console.log(itemlist.children[1]);
+itemlist.children[1].style.backgroundColor = 'yellow';
+
+//FIrstCHild
+//console.log(itemlist.firstChild);
+
+//First Element Child
+console.log(itemlist.firstElementChild);
+itemlist.firstElementChild.textContent = 'Hello 1';
+
+//LastElementChild
+console.log(itemlist.lastElementChild);
+itemlist.lastElementChild.textContent = 'Hello 4';
+ */
+
+//nextSibling
+/* console.log(itemlist.nextSibling);
+//nextElementSibling
+console.log(itemlist.nextElementSibling); */
+
+//previousSibling
+/* console.log(itemlist.previousSibling);
+//prevElementSibling
+console.log(itemlist.previousElementSibling);
+itemlist.previousElementSibling.style.color = 'green'; */
+
+// create Element
+
+//Create a div
+var newDiv = document.createElement('div');
+
+//Add Class
+newDiv.className= 'hello';
+//Add ID
+newDiv.id='hello1';
+//Add Attr
+newDiv.setAttribute('title', 'Hello  Div');
+
+//Create a text node
+var newDivText = document.createTextNode('HEllo word');
+//Add Text to div
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+var firstItem = document.querySelector('#items li:first-child');
+
+var newListItem = document.createElement('li');
+newListItem.textContent = 'HEllo word';
+
+console.log(newDiv);
+
+newDiv.style,fontSize = '30px';
+
+container.insertBefore(newDiv, h1);
+firstItem.parentNode.insertBefore(newListItem, firstItem);
+
