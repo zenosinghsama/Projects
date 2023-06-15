3942446efa67a8bcead4bffd91c981adf575fabb
zenosinghsama <iamarjunsingh007@gmail.com>
2023-06-15 18:30:45 +0530

    Express Add Product parsed

--- Node Projects/Express.js/app.js
+++ Node Projects/Express.js/app.js
@@ -1,19 +1,28 @@
-const http = require('http');
-
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the Middleware!');
-    next(); // Allows the request to continue to next middleware in line
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+   //console.log('In another Middleware!');
+    res.send(`
+    <form action="/product" method = "POST">
+    <input type = "text" name="title" placeholder = "Product Title">
+    <input type = "number" name = "size" placeholder = "Product Size"> 
+    <button type ="submit">Add Product</button></form>`)
 });
 
-app.use((req, res, next) => {
-    console.log('In another Middleware!');
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+   // console.log('In another Middleware!');
     res.send('<h1>Hello from Express!</h1>'); // Allows to send a response and allows to attach body which is of type any
     //res.send({'key1':'value'})
 });
 
 app.listen(3000);
- 
