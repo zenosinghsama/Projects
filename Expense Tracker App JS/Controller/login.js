930451409a3cd5a896d49b8a1ee1528341c0163c
zenosinghsama <iamarjunsingh007@gmail.com>
2023-07-10 14:35:50 +0530

    Password Encryption

--- Expense Tracker App JS/Controller/login.js
+++ Expense Tracker App JS/Controller/login.js
@@ -2,23 +2,28 @@ const path = require('path');
 
 const User = require("../Models/usersignup");
 
+const bcrypt = require('bcrypt');
+
 exports.getLoginForm = (req, res, next) => {
     res.sendFile(path.join(__dirname, "../views", "login.html"));
 }
 
 exports.postLogin = async (req, res, next) => {
     try {
-        const email = req.body.email;
-        const password = req.body.password;
+        const { email, password } = req.body;
 
+        // FInd User by Email
         const user = await User.findOne({where: {email: email } });
 
         if(!user) {
             return res.status(404).json({ message: "User not Found" });
         }
 
-        if(user.password !== password) {
-            return res.status(401).json({ message: "User not Authorized" });
+        // Compare Hashed Password
+        const validPass = await bcrypt.compare(password, user.password);
+
+        if(!validPass) {
+            return res.status(500).json({ message: "User not Authorized" });
         }
         res.status(200).json( { message: "Login Successful" });
     }  catch (err) {
--- Expense Tracker App JS/Controller/signup.js
+++ Expense Tracker App JS/Controller/signup.js
@@ -1,25 +1,30 @@
-const path = require('path');
+const path = require("path");
 
 const User = require("../Models/usersignup");
 
+const bcrypt = require("bcrypt");
+
 exports.getAddUser = (req, res, next) => {
-    res.sendFile(path.join(__dirname, "../views", "Signup.html"));
+  res.sendFile(path.join(__dirname, "../views", "Signup.html"));
 };
 
 exports.postAddUser = async (req, res, next) => {
-    try {
-        const username = req.body.username;
-        const email = req.body.email;
-        const password = req.body.password;
+  try {
+    const { username, email, password } = req.body;
 
-        const user = await User.create ({
-            username: username,
-            email: email,
-            password: password,
-        });
-        res.status(201).json({ newUser: user });
-    }catch (err) {
-        res.status(500).json( { error: err } );
-    }
+    // Generate a Salt -> to use for hashing
+    const salt = await bcrypt.genSalt(10);
+
+    // hash the password
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    await User.create({
+      username: username,
+      email: email,
+      password: hashedPassword
+    });
+    res.status(200).json({ message: 'User created successfully' });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
 };
-
