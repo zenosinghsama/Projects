8b1ce7472f661dfeb54c06bf474cb7d2dcf7e207
zenosinghsama <iamarjunsingh007@gmail.com>
2023-07-17 14:52:27 +0530

    Leaderboard

---
+++ Expense Tracker App/Middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const User = require('../Models/userModel');
+
+exports.authenticateToken = (req, res, next) => {
+
+  try {
+    const token = req.header('Authorization');
+    console.log(token);
+
+    const user = jwt.verify(token, 'cgM1IZO3sD');
+    console.log('userID>>>>', user.userId)
+
+    User.findByPk(user.userId)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    } 
+  catch (err) {
+    res.status(401).json({ success: false, message:'authentication failed'})
+  }
+}
+
